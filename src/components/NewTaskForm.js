fa0baa1f5addd1957d48de229381c5ae260be58c
Daryl Rogony <darylrogony@gmail.com>
2022-05-23 06:27:34 +0300

    Completed Assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState }from "react";
 
-function NewTaskForm() {
-  return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
-    </form>
-  );
-}
+
+function NewTaskForm({ onTaskFormSubmit, categories }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("code")
+
+  function handleSubmit(e) {
+    e.preventdefault();
+    onTaskFormSubmit({ category, text});
+    setCategory("code")
+    setText("")
+  }
+
+  function onChangeText(event) {
+      setText(event.target.value);
+  }
+
+  function onChangeCategory(event) {
+    setCategory(event.target.value);
+  }
+
+    return (
+      <form className="new-task-form" onSubmit={handleSubmit}>
+        <label>
+          Details
+          <input
+          type="text"
+          name="text"
+          onChange={onChangeText}
+          />
+        </label>
+        <label>
+          Category
+          <select name="category" onChange={onChangeCategory}>
+            {/* render <option> elements for each category here */}
+            {categories.map((lists) => {
+              <option key={lists}>{lists}</option>
+            })}
+          </select>
+        </label>
+        <input type="submit" value="Add task" />
+      </form>
+    );
+  }
 
 export default NewTaskForm;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onDeleteTask }) {
+  const tasklist = tasks.map((task) => {
+    <Task
+      key={task.text}
+      category={task.category}
+      text={task.text}
+      onDeleteTask={onDeleteTask}
+    />
+  });
+  
+  return <div className="tasks"> {tasklist} </div>
 }
-
+      // {/* display a list of tasks using Task component */}
 export default TaskList;
